2218de21d620e2f5c793828b78d1ce7fdf162b55
Joe <joseph.as.peterson@gmail.com>
2019-06-26 13:47:50 -0600

    Revised and simplified error checking and resolution

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,11 +14,13 @@ var omxPlayer *helpers.OMXPlayer
 //PlayStream gets a stream url and attempts to switch the omxplayer output to that stream. If no stream is playing, then a new instance of omxplayer is started.
 func PlayStream(ctx echo.Context) error {
 	checkPlayerStatus()
+
 	streamURL := ctx.Param("streamURL")
 	streamURL, err := url.QueryUnescape(streamURL)
 	if err != nil {
 		return ctx.JSON(http.StatusInternalServerError, err.Error())
 	}
+
 	if omxPlayer == nil {
 		err = startNewPlayer(streamURL)
 		if err != nil {
@@ -27,40 +29,32 @@ func PlayStream(ctx echo.Context) error {
 		}
 		return ctx.JSON(http.StatusOK, "Stream player started")
 	}
+
 	err = switchStream(streamURL)
 	if err != nil {
 		//Todo: Log error
 		return ctx.JSON(http.StatusInternalServerError, err.Error())
 	}
+
 	return ctx.JSON(http.StatusOK, "Stream switched")
 }
 
 func startNewPlayer(streamURL string) (err error) {
 	omxPlayer, err = helpers.StartOMX(streamURL)
 	if err != nil {
-		omxPlayer = nil
 		return
 	}
 	err = omxPlayer.WaitForReady()
-	if err != nil {
-		omxPlayer = nil
-		return
-	}
 	return
 }
 
 func switchStream(streamURL string) (err error) {
-	err = omxPlayer.WaitForReady()
-	if err != nil {
-		err = helpers.StopStream(omxPlayer.Connection)
-		omxPlayer = nil
-		return
-	}
 	if checkStream(streamURL) {
 		err = helpers.SwitchStream(omxPlayer.Connection, streamURL)
 		if err != nil {
 			return
 		}
+		err = omxPlayer.WaitForReady()
 	}
 	return
 }
